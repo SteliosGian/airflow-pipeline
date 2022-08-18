3d311c936ea248e3ed2647dcaafb2da9b3984930
SteliosGian <steliosgiannik@gmail.com>
2022-08-18 15:53:51 +0300

    Update dags

--- src/dags/dag.py
+++ src/dags/dag.py
@@ -27,25 +27,12 @@ spark.sparkContext.addPyFile("jobs/process_label.py")
 spark.sparkContext.addPyFile("jobs/process_temperature.py")
 
 with DAG(
-    'process_data',
+    'process_data_local',
     default_args={
         'depends_on_past': False,
         'email': ['steliosgiannik@gmail.com'],
         'email_on_failure': False,
-        'email_on_retry': False,
-        # 'retries': 1,
-        # 'queue': 'bash_queue',
-        # 'pool': 'backfill',
-        # 'priority_weight': 10,
-        # 'end_date': datetime(2016, 1, 1),
-        # 'wait_for_downstream': False,
-        # 'sla': timedelta(hours=2),
-        # 'execution_timeout': timedelta(seconds=300),
-        # 'on_failure_callback': some_function,
-        # 'on_success_callback': some_other_function,
-        # 'on_retry_callback': another_function,
-        # 'sla_miss_callback': yet_another_function,
-        # 'trigger_rule': 'all_success'
+        'email_on_retry': False
     },
     description='Process data',
     start_date=datetime(2021, 1, 1),
