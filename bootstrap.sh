ff0e997e0ab75e5d235a9f835135365448a2a33a
SteliosGian <steliosgiannik@gmail.com>
2022-08-19 14:50:40 +0300

    Add bootstrap script for emr

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+BUCKET_NAME=st-proj-airflow-bucket-data-eng
+
+set -x 
+ 
+echo '-----------RUNNING BOOTSTRAP------------------------'
+ 
+echo '-----------COPYING REQUIREMENTS FILE LOCALLY--------'
+ 
+aws s3 cp s3://${BUCKET_NAME}/job_requirements.txt .
+ 
+echo '-----------INSTALLING REQUIREMENTS------------------'
+ 
+sudo python3 -m pip install -r job_requirements.txt
+ 
+echo '-----------DONE BOOTSTRAP---------------------------'
